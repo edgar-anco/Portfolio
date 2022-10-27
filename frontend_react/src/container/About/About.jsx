0ecf1cd730cea72abad3ab7209732aeb783131bd
Edgar Andre Anco Copaja <89050053+edgar-anco@users.noreply.github.com>
2022-10-26 22:32:43 -0500

    feat: added higher-order components

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-//import { AppWrap, MotionWrap } from '../../wrapper';
-//import { images } from '../../constants'
+import { AppWrap } from '../../wrapper';
 import './About.scss';
 import { urlFor, client } from '../../client';
 
@@ -41,7 +40,7 @@ const About = () => {
   )
 }
 
-export default About
+export default AppWrap(About, 'about');
 
 /*
 export default AppWrap(
